d2f2a09c8f4506f6331cc817547a05b496053f2e
Jean Mauricio <poolgamer0920u@gmail.com>
2021-10-13 19:38:10 -0400

    arreglado el borde del titulo

--- src/components/Navegacion.js
+++ src/components/Navegacion.js
@@ -5,12 +5,19 @@ class Navigation extends Component{
     render(){
         return(
             
+                
                 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                    <a class="navbar-brand" href="#">                    
-                    BETAWEB
+                    
+                    <a class="navbar-brand" href="#" border-width="medium">                    
+                      BETAWEB
                     <img src="https://previews.123rf.com/images/mochipet/mochipet1707/mochipet170700056/82167896-ilustraci%C3%B3n-vectorial-bombilla-con-haces-de-luz-en-estilo-pop-art-dibujos-animados-con-contorno-aisl.jpg" width="30" height="30" class="d-inline-block align-top" alt=""/>                       
-                </a>
-            </nav>
+                    
+                    </a>
+                    
+                    
+                    
+                </nav>
+                
             
         )
     }
