50874702ef543f9c970b8208eb5c0fbe9c742fae
Jean Mauricio <poolgamer0920u@gmail.com>
2021-10-13 15:14:29 -0400

    agregado naavbar e icono

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
-  );
+import './App.css';
+import Navigation from './components/Navegacion';
+
+class App extends Component {
+  render(){
+     return (
+      <div className="App">
+        <Navigation />
+       
+      </div>
+    );
+  }
 }
 
 export default App;
