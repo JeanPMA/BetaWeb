857ca89f385cbb734d66797928885604928c060c
RodrigoGandarillas <rodrigo.gandarillas.heredia@gmail.com>
2021-10-14 21:43:36 -0400

    correcion contenedor de curso y tamaña de fuente

--- src/components/OfCurso.js
+++ src/components/OfCurso.js
@@ -5,13 +5,17 @@ function OfCurso() {
   return (
     <a href="https://www.udemy.com/">
       <div
-        className="card  border-top border-start border-end border-bottom border-white p-2 text-white"
+        className="card border-top border-start border-end border-bottom border-white p-2 text-white"
         id="ofertas"
       >
         <img src={imagenCurso} />
         <div className="card-body">
-          <h4 className="card-title">Titulo del curso</h4>
-          <p className="card-text ">Descripcion del curso</p>
+          <h4 id="titulo" className="card-title">
+            Titulo del curso
+          </h4>
+          <p id="descripcion" className="card-text ">
+            Descripcion del curso
+          </p>
         </div>
       </div>
     </a>
