7928d8cc35f2f7095b3e86480a079f18e03a2547
Jean Mauricio <poolgamer0920u@gmail.com>
2021-10-16 15:19:48 -0400

    agregado barra buscador

--- src/components/Navegacion.js
+++ src/components/Navegacion.js
@@ -11,12 +11,13 @@ class Navigation extends Component{
                     
                     <a class="navbar-brand" href="#" border-width="medium">                    
                       BETAWEB
-                    <img src="https://previews.123rf.com/images/mochipet/mochipet1707/mochipet170700056/82167896-ilustraci%C3%B3n-vectorial-bombilla-con-haces-de-luz-en-estilo-pop-art-dibujos-animados-con-contorno-aisl.jpg" width="30" height="30" class="d-inline-block align-top" alt=""/>                       
-                    
+                    <img src="https://previews.123rf.com/images/mochipet/mochipet1707/mochipet170700056/82167896-ilustraci%C3%B3n-vectorial-bombilla-con-haces-de-luz-en-estilo-pop-art-dibujos-animados-con-contorno-aisl.jpg" width="30" height="30" class="d-inline-block align-top" alt=""/>                        
                     </a> 
-                  
+
+                    <input class="buscador" type="search" id="search" placeholder="Buscar cursos" />   
+
                     <Link to="/cursos" id="boton_buscar" className="btn " type="button">Cursos</Link>                    
-                    
+                                       
                 </nav>
                 
             
