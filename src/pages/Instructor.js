7d3d3197dee86afcb06ed70e82f1e5f14cd29376
Rodrigo Gandarillas Heredia <rodrigo.gandarillas.heredia@gmail.com>
2021-10-18 12:40:32 -0400

    implementacion menu desplegable pantalla de instructor

--- src/pages/Instructor.js
+++ src/pages/Instructor.js
@@ -4,47 +4,15 @@ import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHome } from "@fortawesome/free-solid-svg-icons";
 import { faPlayCircle } from "@fortawesome/free-regular-svg-icons";
+import NavegacionInstructor from "../components/NavegacionInstructor";
 
 function Instructor() {
   return (
     <div className="container-instructor">
-      <div className="aside">
-        <div className="foto-y-nombre">
-          <img
-            src={ImagenUsuario}
-            alt="foto de perfil"
-            className="foto-usuario"
-          />
-          <h3 className="nombre-usuario">Nombre del usuario</h3>
-        </div>
-        <div className="menu-instructor">
-          <h2 className="texto-menu">Menú</h2>
-          <div className="botones-menu-instructor">
-            <Link
-              to="/instructor/#"
-              type="button"
-              className="boton-inicio-instructor text-white"
-            >
-              <div className="icono-inicio-cursos-instructor">
-                <FontAwesomeIcon icon={faHome} />
-              </div>
-
-              <span>Inicio</span>
-            </Link>
-
-            <Link
-              to="/instructor/#"
-              type="button"
-              className="boton-cursos-instructor  text-white"
-            >
-              <div className="icono-inicio-cursos-instructor icono-boton-instructor">
-                <FontAwesomeIcon icon={faPlayCircle} />{" "}
-              </div>
-              <span>Mis Cursos</span>
-            </Link>
-          </div>
-        </div>
+      <div className="navegacion-menu">
+        <NavegacionInstructor />
       </div>
+      <div className="contenido-pagina"></div>
     </div>
   );
 }
