f07f0ce31298f46a77ed16eacb37bbc720671e34
Rodrigo Gandarillas Heredia <rodrigo.gandarillas.heredia@gmail.com>
2021-10-18 12:43:58 -0400

    correccion menu instructor

--- src/pages/Instructor.js
+++ src/pages/Instructor.js
@@ -1,9 +1,4 @@
 import React from "react";
-import ImagenUsuario from "../assets/usuario.png";
-import { Link } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHome } from "@fortawesome/free-solid-svg-icons";
-import { faPlayCircle } from "@fortawesome/free-regular-svg-icons";
 import NavegacionInstructor from "../components/NavegacionInstructor";
 
 function Instructor() {
