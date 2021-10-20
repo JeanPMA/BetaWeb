f6033cc874ca9bbeda87240f88020a37c7918bd5
RodrigoGandarillas <rodrigo.gandarillas.heredia@gmail.com>
2021-10-20 15:01:33 -0400

    llenado de contenido slider de la pagina principal del instructor

--- src/components/SliderInstructor.js
+++ src/components/SliderInstructor.js
@@ -1,4 +1,12 @@
+import React from "react";
 import Carousel from "./CarouselInstructor";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faSearch,
+  faEye,
+  faPencilAlt,
+  faTrash,
+} from "@fortawesome/free-solid-svg-icons";
 const SliderInstructor = () => {
   return (
     <div>
@@ -27,13 +35,43 @@ const SliderInstructor = () => {
         >
           <Carousel show={3}>
             <div className=" border border-white consejo">
-              <div className="justify-content-center pt-2 imagen-oferta"></div>
+              <div className="justify-content-center pt-2 imagen-oferta">
+                <div className="icono1-slider-instructor">
+                  <FontAwesomeIcon icon={faSearch} />
+                  <div className="eye">
+                    <FontAwesomeIcon icon={faEye} />
+                  </div>
+                  <div>
+                    <p className="texto-1">Permite ver el estado de un curso</p>
+                  </div>
+                </div>
+              </div>
             </div>
             <div className="border border-white consejo">
-              <div className="justify-content-center pt-2 imagen-oferta"></div>
+              <div className="justify-content-center pt-2 imagen-oferta">
+                <div className="icono2-slider-instructor">
+                  <FontAwesomeIcon icon={faPencilAlt} />
+
+                  <div>
+                    <p className="texto-2">
+                      Permite editar un curso ya registrado
+                    </p>
+                  </div>
+                </div>
+              </div>
             </div>
             <div className="contenedor-cursos-inicio border border-white consejo">
-              <div className="justify-content-center pt-2 imagen-oferta "></div>
+              <div className="justify-content-center pt-2 imagen-oferta ">
+                <div className="icono3-slider-instructor">
+                  <FontAwesomeIcon icon={faTrash} />
+
+                  <div>
+                    <p className="texto-3">
+                      Permite eliminar un curso ya registrado
+                    </p>
+                  </div>
+                </div>
+              </div>
             </div>
             <div className=" border border-white consejo">
               <div className="justify-content-center pt-2 imagen-oferta "></div>
