fa212e4d9485f1f8d83bf4901efadeaa1766bae3
Jean Mauricio <poolgamer0920u@gmail.com>
2021-10-30 12:18:31 -0400

    arreglado bug del buscador

--- src/Vistados/Vista.js
+++ src/Vistados/Vista.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Cursoss from "./Cursoss";
+import logoLupa from "../imagenes/logoLupa.png";
+import axios from 'axios';
 
 function Vista() {
   const [cursos, setCursos] = useState([]);
@@ -15,8 +17,47 @@ function Vista() {
     fetchCursos("https://betaweb-back.herokuapp.com/api/curso");
   }, []);
 
+  const [tabla, setTablaCursos] = useState([]);
+  const [busqueda, setBusqueda]=useState("");
+
+  const peticionGet=async()=>{
+    await axios.get("https://betaweb-back.herokuapp.com/api/curso")
+    .then(response=>{
+      setTablaCursos(response.data);  
+      setCursos(response.data);    
+    }).catch(error=>{
+      console.log(error);
+    })
+  }
+  const hanldeChange=e=>{
+    setBusqueda(e.target.value);
+    filtrar(e.target.value);
+  }
+  const filtrar=(terminoBusqueda)=>{
+    var resultadoBusqueda=tabla.filter((elemento)=>{
+      if(elemento.nombre.toString().toLowerCase().includes(terminoBusqueda.toLowerCase())
+      || elemento.descripcion.toString().toLowerCase().includes(terminoBusqueda.toLowerCase())
+      ){
+        return elemento;
+      }else{
+        <a> no se encontro nada relacionado</a>
+      }
+    });
+    setCursos(resultadoBusqueda);
+  }
+  React.useEffect(()=>{
+    peticionGet();
+  },[])
+
   return (
     <div>
+      <div>
+           <input class="buscador" type="search" id="search" 
+           placeholder="Buscar cursos" value={busqueda} 
+           onChange={hanldeChange}
+           />   
+              <img src={logoLupa} id="logo_lupa" width="30" height="30"/>
+      </div>
       <div className="container scroll-container" id="cursosRegistrados">
         <Cursoss cursos={cursos} />
       </div>
--- src/components/Navegacion.js
+++ src/components/Navegacion.js
@@ -14,8 +14,7 @@ class Navigation extends Component{
                     <img src="https://previews.123rf.com/images/mochipet/mochipet1707/mochipet170700056/82167896-ilustraci%C3%B3n-vectorial-bombilla-con-haces-de-luz-en-estilo-pop-art-dibujos-animados-con-contorno-aisl.jpg" width="30" height="30" class="d-inline-block align-top" alt=""/>                        
                     </a> 
 
-                    <input class="buscador" type="search" id="search" placeholder="Buscar cursos" />   
-                    <img src={logoLupa} id="logo_lupa" width="30" height="30"/>
+                 
 
                     <Link to="/cursos" id="boton_buscar" className="btn " type="button">Cursos</Link>                    
 
