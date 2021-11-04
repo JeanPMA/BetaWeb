a8d296d76c1cb95f607bb7286cf6b7d8d0da6278
RodrigoGandarillas <rodrigo.gandarillas.heredia@gmail.com>
2021-11-04 14:52:41 -0400

    actualizacion nueva url paraq cursos

--- src/Vistados/Vista.js
+++ src/Vistados/Vista.js
@@ -1,59 +1,71 @@
 import React, { useEffect, useState } from "react";
 import Cursoss from "./Cursoss";
 import logoLupa from "../imagenes/logoLupa.png";
-import axios from 'axios';
+import axios from "axios";
 
 function Vista() {
   const [cursos, setCursos] = useState([]);
 
   const fetchCursos = () => {
-    fetch("https://betaweb-back.herokuapp.com/api/curso")
+    fetch("https://app-cleancode.herokuapp.com/api/curso")
       .then((response) => response.json())
       .then((data) => setCursos(data))
       .catch((error) => console.log(error));
   };
 
   useEffect(() => {
-    fetchCursos("https://betaweb-back.herokuapp.com/api/curso");
+    fetchCursos("https://app-cleancode.herokuapp.com/api/curso");
   }, []);
 
   const [tabla, setTablaCursos] = useState([]);
-  const [busqueda, setBusqueda]=useState("");
+  const [busqueda, setBusqueda] = useState("");
 
-  const peticionGet=async()=>{
-    await axios.get("https://betaweb-back.herokuapp.com/api/curso")
-    .then(response=>{
-      setTablaCursos(response.data);  
-      setCursos(response.data);    
-    }).catch(error=>{
-      console.log(error);
-    })
-  }
-  const hanldeChange=e=>{
+  const peticionGet = async () => {
+    await axios
+      .get(
+        "https://app-cleancode.herokuapp.com/api/cursohttps://app-cleancode.herokuapp.com/api/curso"
+      )
+      .then((response) => {
+        setTablaCursos(response.data);
+        setCursos(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  const hanldeChange = (e) => {
     setBusqueda(e.target.value);
     filtrar(e.target.value);
-  }
-  const filtrar=(terminoBusqueda)=>{
-    var resultadoBusqueda=tabla.filter((elemento)=>{
-      if(elemento.nombre.toString().toLowerCase().includes(terminoBusqueda.toLowerCase())
-      ){
+  };
+  const filtrar = (terminoBusqueda) => {
+    var resultadoBusqueda = tabla.filter((elemento) => {
+      if (
+        elemento.nombre
+          .toString()
+          .toLowerCase()
+          .includes(terminoBusqueda.toLowerCase())
+      ) {
         return elemento;
       }
     });
     setCursos(resultadoBusqueda);
-  }
-  React.useEffect(()=>{
+  };
+  React.useEffect(() => {
     peticionGet();
-  },[])
+  }, []);
 
   return (
     <div>
       <div>
-           <input class="buscador" type="search" id="search" 
-           placeholder="Introduzca palabra clave" value={busqueda} 
-           onChange={hanldeChange}
-           />   
-              <img src={logoLupa} id="logo_lupa" width="30" height="30"/>
+        <input
+          class="buscador"
+          type="search"
+          id="search"
+          placeholder="Introduzca palabra clave"
+          value={busqueda}
+          onChange={hanldeChange}
+        />
+        <img src={logoLupa} id="logo_lupa" width="30" height="30" />
       </div>
       <div className="container scroll-container" id="cursosRegistrados">
         <Cursoss cursos={cursos} />
