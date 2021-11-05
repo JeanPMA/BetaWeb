4799f5987b53e890305d84284634f44cb0171a1b
60717466 <60717466>
2021-11-04 20:05:51 -0400

    Lista de instructores

--- src/misCursosinst/Lista.js
+++ src/misCursosinst/Lista.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from "react";
 import "./Vista.css";
 import Lupa from "../assets/lupa.png";
@@ -82,6 +83,87 @@ const Lista = () => {
       </div>
     </div>
   );
+=======
+import React, { useEffect, useState } from "react";
+import "./Vista.css"
+import axios from 'axios';
+import Lupa from "../assets/lupa.png";
+
+const lupa = <img src={Lupa} className="lupa-2" />;
+
+function Lista() {
+
+  const [instructor, setInstructor] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://betaweb-back.herokuapp.com/api/instructor")
+      .then(response => {
+        setInstructor(response.data);
+      }).catch(error => {
+        console.log(error);
+      })
+  }, []);
+
+  const getInstructorByid = () => {
+    const idInstructor = parseInt(getCookies("id_usuario"));
+    let instruc = {};
+    instructor.forEach(ins => {
+      if (ins.id_instructor === idInstructor) {
+        instruc = ins;
+      }
+    })
+    return instruc;
+  };
+
+  const getCookies = (cname) => {
+    var name = cname + '=';
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return '';
+  };
+
+  return (
+    <div className="list-group">
+      <div className="col-md-6 col align-self-center">
+        <div className="container">
+          <h3 className="text-white">Mis cursos</h3>
+          <table className="table text-white" >
+            <thead>
+              <tr>
+                <th scope="col" className="text-white">Nombres</th>
+                <th scope="col" className="text-white">Acciones</th>
+              </tr>
+            </thead>
+            <tbody>
+              {instructor.length > 0 && getInstructorByid().cursos?.map(element => {
+                return (<tr>
+                  <td className="text-white">{element.nombre}</td>
+                  <td>
+                    <button className="btn btn-lg"
+                      data-bs-toggle="modal"
+                      data-bs-target="#exampleModal"
+                      data-bs-whatever="@mdo" type="button"> {lupa}</button>
+                    <button className="btn btn-lg" type="button"><i className="bi bi-trash-fill rojo"></i></button>
+                  </td>
+                </tr>);
+              })
+              }
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  )
+>>>>>>> Lista de instructores
 };
 
 export default Lista;
