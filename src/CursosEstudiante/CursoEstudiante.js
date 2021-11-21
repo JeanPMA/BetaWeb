7e7ce42bfff2553680b18dfd3fdf9e5ad7b1ef19
60717466 <60717466>
2021-11-20 23:07:28 -0400

    no permitir registrarse a un curso nuevamente

--- src/CursosEstudiante/CursoEstudiante.js
+++ src/CursosEstudiante/CursoEstudiante.js
@@ -1,10 +1,61 @@
-import React, { useState } from "react";
+import React, { useState ,useEffect} from "react";
 import InscribirCurso from "./InscribirCurso";
 import MisVideo from "../misCursosEstudiante/MisVideo";
 import "./InscribirCurso.css";
+import axios from "axios";
 
 const CursoEstudiante = ({ cursos = [] }) => {
   const [curso, setCurso] = useState({});
+  const [miscursos, setMiscursos] = useState([]);
+
+  useEffect(() => {
+    if (miscursos.length === 0) {
+      axios
+        .get("https://betaweb-back.herokuapp.com/api/inscritos")
+        .then((response) => {
+        setMiscursos(
+            response.data.map((element) => {
+              if (
+                element.estudiante?.id_estudiante ===
+                parseInt(getCookies("id_estudiante"))
+              ) {
+                return element.curso;
+              }
+            })
+          );
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  }, []);
+
+  const buscador = (idcurso) => {
+    let result=false
+    miscursos.forEach(element => {
+      if(element && idcurso === element.id_curso){
+         result=true;
+      }
+    });
+    return result;
+  }
+
+  const getCookies = (cname) => {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(";");
+    for (var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == " ") {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  };
+
   return (
     <div>
       {cursos.map((item) => (
@@ -54,15 +105,26 @@ const CursoEstudiante = ({ cursos = [] }) => {
               {item.descripcion}
             </p>
             <div class="d-grid justify-content-md-end">
+              {
+               !buscador(item.id_curso) ? 
               <button
                 type="button"
                 onClick={() => setCurso(item)}
                 data-bs-toggle="modal"
                 data-bs-target="#staticBackdrop"
-                className="  btnnn btn-outline-success boton-inscribirse  "
+                className="btnnn btn-outline-success boton-inscribirse"
               >
                 Inscribirse
               </button>
+              :
+              <button
+                type="button"
+                style={{color:"yellow"}}
+                className="btnnn btn-outline-warning boton-inscribirse"
+              >
+                Inscrito
+              </button>
+              }
             </div>
           </div>
         </div>
