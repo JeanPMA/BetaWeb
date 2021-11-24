49d3cc4eb8c8744835fc7b7dd5cc7e32d5a32bb5
Rodrigo Gandarillas <rodrigo.gandarillas.heredia@gmail.com>
2021-11-23 20:03:48 -0400

    detalles de curso en modal

---
+++ src/components/DetallesModal.js
@@ -0,0 +1,49 @@
+import React from "react";
+import "../misCursosEstudiante/Vista.css";
+
+function DetallesModal(props) {
+  return (
+    <div
+      className="modal fade"
+      id="videoModal"
+      tabindex="-1"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+      data-bs-backdrop="static"
+      data-bs-keyboard="false"
+    >
+      <div className="modal-dialog modal-dialog-centered modal-lg">
+        <div className="modal-content color">
+          <div className="modal-header">
+            <h5 className="modal-title text-white" id="exampleModalLabel">
+              {props.curso.nombre}
+            </h5>
+            <button
+              type="button"
+              className="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div className="modal-body">
+            <img
+              src={props.curso.ubicacion_img}
+              width="50%"
+              className="m-auto"
+            />
+            <p className="text-white fs-5 fw-bold text-start ">
+              {props.curso.instructor.nombre +
+                " " +
+                props.curso.instructor.apellido_paterno +
+                " " +
+                props.curso.instructor.apellido_materno}
+            </p>
+            <p className="text-white text-start">{props.curso.descripcion}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default DetallesModal;
