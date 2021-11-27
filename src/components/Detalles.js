f3c25696b270f193f03c5353e269c5bd1a45379d
Rodrigo Gandarillas <rodrigo.gandarillas.heredia@gmail.com>
2021-11-26 20:16:49 -0400

    correccion centrado de modal

--- src/components/Detalles.js
+++ src/components/Detalles.js
@@ -1,28 +1,28 @@
-import React, { Component } from 'react';
-import logotipo from "../imagenes/logotipoFinal.png"
-class Detalles extends Component{
-    render(){
-        return(
-            
-                <div class="Descripcion">
-                    <h3 class="Titulo"> ACERCA DE NOSOTROS... <pre></pre> </h3>    
-                    <p> BETAWEB es una plataforma de cursos gratuitos,  <br></br>
-                     dirigida al área de ciencias de la computación
-                    </p>
-                    
-                    <div class="logo">
-                        <a>
-                        <img src={logotipo} width="320" height="200"/>
-                        
-                        </a>
-                        </div>                    
-                
-                </div>
-                
-                
-            
-        )
-    }
+import React, { Component } from "react";
+import logotipo from "../imagenes/logotipoFinal.png";
+class Detalles extends Component {
+  render() {
+    return (
+      <>
+        <div class="Descripcion">
+          <h3 class="Titulo">
+            {" "}
+            ACERCA DE NOSOTROS... <pre></pre>{" "}
+          </h3>
+          <p>
+            {" "}
+            BETAWEB es una plataforma de cursos gratuitos, <br></br>
+            dirigida al área de ciencias de la computación
+          </p>
+        </div>
+        <div class="logo">
+          <a>
+            <img src={logotipo} width="320" height="200" />
+          </a>
+        </div>
+      </>
+    );
+  }
 }
 
 export default Detalles;
